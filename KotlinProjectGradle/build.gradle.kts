c95fe5f12f7c63cf2cfcefbc5b1a05327da87564
Mehdi Medkour <80688638+mehdichappas@users.noreply.github.com>
2025-12-10 23:21:41 -0500

    Initial commit

---
+++ KotlinProjectGradle/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(kotlin("reflect"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
